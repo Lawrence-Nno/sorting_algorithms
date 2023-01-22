9998b3764e8761b9c1e5feba89a00ad07c9a91c4
Lawrence Nno <Lawrence.Nno@gmail.com>
2023-01-22 14:56:12 +0100

    Added task 1 files

--- sort.h
+++ sort.h
@@ -40,5 +40,6 @@ void print_list(const listint_t *list);
 
 /* sorting algorithms prototypes given */
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif /*_SORT_H_*/
